946a242024ebdae8d06ab1cd3cfa0e4bd0b28d0a
DreadSK <93290083+DreadSK@users.noreply.github.com>
2021-11-10 08:44:14 -0600

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,11 @@ public class Hello {
     public static void main(String[] args) {  
         System.out.println("Hello World");
         System.out.println("Hello Landon");
+        print("Whats up Landon!");
+        print("Hello Scott");
+    }
+    
+    public static void print(String input) {
+        System.out.println(input);
     }
 }
