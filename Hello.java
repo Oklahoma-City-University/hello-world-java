efb922426c34d4cb18b49d15d348ad96326c00e6
Deathghat <89095218+Deathghat@users.noreply.github.com>
2021-11-22 15:21:46 -0600

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {  
         System.out.println("Hello World");
         System.out.println("Hello Landon");
+        System.out.println("Hello Tyler");
         print("Whats up Landon!");
         print("Hello Scott");
     }
