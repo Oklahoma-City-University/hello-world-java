a9032d89ba5385394ca932e8ceb23765e312a18d
Jeff Maxwell <jeff@tcnsolutions.com>
2023-10-08 09:42:13 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,12 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Maxwell");
         System.out.println("Hello World");
-
-        System.out.println("Hello Maxwell Again");
-
-        System.out.println("Hello World Again");
-
-        System.out.println("Hello Maxwell Again Again");
     }
 }
