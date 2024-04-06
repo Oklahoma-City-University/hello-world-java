ead6daa9acfb4ab63346b06ab303c67985aa9949
DINESH GUDE <gudedinesh2426@gmail.com>
2024-04-05 23:45:17 -0500

    Added new hello line

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello Shaik Reshma");
-        System.out.println("Hello Shaik Reshma again");
+        System.out.println("Hello Kadiri Vivek");
     }
 }
