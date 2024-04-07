fa9817b89843fb9e70d5ee16439588337052caf6
SOUREESHVARAMAY <syarakaraju@my.okcu.edu>
2024-04-07 15:52:03 -0500

    added hello soureesh

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello Kadiri Vivek");
+        System.out.println("Hello Soureesh Varma Yarakaraju ");
+        System.out.println("thank You");
     }
 }
